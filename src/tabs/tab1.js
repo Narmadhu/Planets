d71ae8c1bf6366daaadd2e8287d416d3261b53b6
Narmadhu <oknarmadhu10496@gmail.com>
2020-06-02 21:34:01 +0530

    fetch data from API

--- src/tabs/tab1.js
+++ src/tabs/tab1.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Planets() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://assignment-machstatz.herokuapp.com/planet"
+    );
+    const items = await data.json();
+    console.log(items);
+    setItems(items);
+  };
+
   return (
-    <div className="Planets">
-      <h3>Planet List</h3>
+    <div>
+      {items.map((item) => (
+        <h2 key={item.id}>{item.name}</h2>
+      ))}
     </div>
   );
 }
