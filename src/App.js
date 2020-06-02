186142760ef5b7527a3946d748362e6286abe783
Narmadhu <oknarmadhu10496@gmail.com>
2020-06-02 18:38:13 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import "./App.css";
 import Nav from "./Nav";
 import Planets from "./tabs/tab1";
 import FovouriteList from "./tabs/tab2";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Planets />
-      <FovouriteList />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/planets" component={Planets} />
+          <Route path="/favouriteList" component={FovouriteList} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Homepage</h1>
+    </div>
+  );
+};
 
 export default App;
--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
+  const tabStyle = {
+    color: "black",
+  };
   return (
     <div>
       <nav>
         <h1>My Project</h1>
         <ul className="Nav-tabs">
-          <li>PlanetList</li>
-          <li>My Favourites</li>
+          <Link to="/planets" style={tabStyle}>
+            <li>PlanetList</li>
+          </Link>
+          <Link to="/favouritelist" style={tabStyle}>
+            <li>My Favourites</li>
+          </Link>
         </ul>
       </nav>
     </div>
